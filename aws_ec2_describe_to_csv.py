d8b7272a43bc51d73d1c07b5cfcd9ec3f3a7aad8
Daniel Asyraaf <suhaimi.asyraaf@gmail.com>
2023-04-04 15:44:51 +0800

    Update aws_ec2_describe_to_csv.py

--- aws_ec2_describe_to_csv.py
+++ aws_ec2_describe_to_csv.py
@@ -1,7 +1,54 @@
-#!/usr/bin/env python3
 import argparse
 import boto3
 import csv
+from datetime import datetime, timedelta
+
+def get_cpu_utilization(instance_id, region):
+    # Create a CloudWatch client
+    cloudwatch = boto3.client('cloudwatch', region_name=region)
+
+    # Set the metric dimensions and period
+    dimensions = [
+        {
+            'Name': 'InstanceId',
+            'Value': instance_id
+        },
+    ]
+    period = 300
+
+    # Set the start and end times for the metric data
+    end_time = datetime.utcnow()
+    start_time = end_time - timedelta(days=7)
+
+    # Retrieve the CPU utilization metric data
+    response = cloudwatch.get_metric_data(
+        MetricDataQueries=[
+            {
+                'Id': 'cpu',
+                'MetricStat': {
+                    'Metric': {
+                        'Namespace': 'AWS/EC2',
+                        'MetricName': 'CPUUtilization',
+                        'Dimensions': dimensions
+                    },
+                    'Period': period,
+                    'Stat': 'Average'
+                },
+                'ReturnData': True
+            },
+        ],
+        StartTime=start_time,
+        EndTime=end_time
+    )
+
+    # Calculate the average CPU utilization and return the result
+    values = response['MetricDataResults'][0]['Values']
+    if len(values) > 0:
+        average = sum(values) / len(values)
+        return '{:.2f}%'.format(round(average, 2))
+    else:
+        return 'N/A'
+
 
 def describe_instances_to_csv(output_file):
     region_name = 'ap-southeast-1'
@@ -10,10 +57,10 @@ def describe_instances_to_csv(output_file):
 
     # Open the output file for writing in CSV format
     with open(output_file, mode='w', newline='') as csv_file:
-        fieldnames = ['Instance ID', 'Tag Name', 'Availability Zone', 'Platform Details', 'Instance Type', 'State', 'VPC ID', 'Private IP Address', 'Public IP Address']
+        fieldnames = ['Instance ID', 'Instance Name', 'Availability Zone', 'Platform Details', 'Instance Type', 'State', 'VPC ID', 'Private IP Address', 'Public IP Address', 'CPU Utilization']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-
+        
         # Extract the fields for each instance object in the response and write to the CSV file
         for reservation in response['Reservations']:
             for instance in reservation['Instances']:
@@ -31,25 +78,31 @@ def describe_instances_to_csv(output_file):
                 vpc_id = instance['VpcId']
                 private_ip_address = instance['PrivateIpAddress']
                 public_ip_address = instance.get('PublicIpAddress', 'N/A')
+                cpu_utilization = get_cpu_utilization(instance_id, region_name)
                 
                 # Write the extracted values to the CSV file
                 writer.writerow({
                     'Instance ID': instance_id,
-                    'Tag Name': tag_name,
+                    'Instance Name': tag_name,
                     'Availability Zone': availability_zone,
                     'Platform Details': platform_details,
                     'Instance Type': instance_type,
                     'State': state,
                     'VPC ID': vpc_id,
                     'Private IP Address': private_ip_address,
-                    'Public IP Address': public_ip_address
+                    'Public IP Address': public_ip_address,
+                    'CPU Utilization': cpu_utilization
                 })
-
-if __name__ == '__main__':
+                
+def main():
     # Parse the command-line arguments
-    parser = argparse.ArgumentParser(description='Script to describe EC2 instances and output to CSV.')
-    parser.add_argument('--output', dest='output_file', default='output.csv', help='Output CSV filename')
+    parser = argparse.ArgumentParser(description='Describe EC2 instances and write to a CSV file.')
+    parser.add_argument('--output', metavar='output_file', type=str, default='output.csv', help='the output file name in CSV format')
     args = parser.parse_args()
 
-    # Call the function to describe instances and output to CSV
-    describe_instances_to_csv(args.output_file)
+    # Run the EC2 describe instances command and write to the output file
+    describe_instances_to_csv(args.output)
+
+if __name__ == '__main__':
+    main()
+
